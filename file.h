a33fc473f2cd1cbfdb1e7eb1f7f5135ce624a728
Henry Hu <henry.hu.sh@gmail.com>
2015-06-07 00:51:48 -0700

    file struct

---
+++ file.h
@@ -0,0 +1,49 @@
+#ifndef __GPHOTOFS2_FILE_H_
+#define __GPHOTOFS2_FILE_H_
+
+#include <string>
+#include <gphoto2/gphoto2.h>
+
+#include "utils.h"
+
+struct File {
+    std::string name;
+    CameraFile *camFile;
+    char *buf;
+    off_t size;
+//    bool writeable;
+    int mtime;
+    int ref;
+    bool changed;
+
+    File(const std::string& name, const CameraFileInfo& info) {
+        this->name = name;
+        mtime = info.file.mtime;
+        size = info.file.size;
+        buf = NULL;
+        ref = 0;
+        changed = false;
+        camFile = nullptr;
+    }
+
+    File(const std::string& name) {
+        this->name = name;
+        mtime = Now();
+        size = 0;
+        buf = NULL;
+        ref = 0;
+        changed = false;
+        camFile = nullptr;
+    }
+
+    ~File() {
+        if (ref > 0) {
+            Error("deleting active file");
+            if (buf) delete[] buf;
+            if (camFile) gp_file_unref(camFile);
+        }
+    }
+};
+
+
+#endif // __GPHOTOFS2_FILE_H_
